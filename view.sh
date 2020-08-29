19131979cf8791208cc162bc3bc509ca714aeec2
Dima <z-dima@live.ru>
2020-08-29 22:24:14 +0300

    no message

--- view.sh
+++ view.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-sudo mkdir -p /view/prometheus; sudo chown 772:772 /view/prometheus
-sudo mkdir /view/alertmanager; sudo chown 772:772 /view/alertmanager
-sudo mkdir /view/grafana_provisioning; sudo chown 772:772 /view/grafana_provisioning
-sudo mkdir /view/grafana; sudo chown 772:772 /view/grafana
-sudo docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
-rm view.sh
+#!/usr/bin/env bash
+mkdir -p /view/prometheus; chown 772:772 /view/prometheus
+mkdir /view/alertmanager; chown 772:772 /view/alertmanager
+mkdir /view/grafana_provisioning; chown 772:772 /view/grafana_provisioning
+mkdir /view/grafana; chown 772:772 /view/grafana
+docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
