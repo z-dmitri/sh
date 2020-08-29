7d01989304c74f6dde5219fbc85c346bc0df1560
Dima <z-dima@live.ru>
2020-08-29 21:57:00 +0300

    no message

--- view.sh
+++ view.sh
@@ -3,3 +3,4 @@ mkdir /view/alertmanager; sudo chown 772:772 /view/alertmanager
 mkdir /view/grafana_provisioning; sudo chown 772:772 /view/grafana_provisioning
 mkdir /view/grafana; sudo chown 772:772 /view/grafana
 sudo docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
+rm view.sh
